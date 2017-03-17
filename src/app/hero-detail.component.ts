cfacb23e0eb8bd6ff9c197894682400462b6d12c
tuyenv <bitworkvn@gmail.com>
2017-03-17 14:19:28 +0700

    http

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location }               from '@angular/common';
 
-import { Hero }         from './hero';
-import { HeroService }  from './hero.service';
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
+
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
@@ -24,6 +25,11 @@ export class HeroDetailComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
   }
 
   goBack(): void {
